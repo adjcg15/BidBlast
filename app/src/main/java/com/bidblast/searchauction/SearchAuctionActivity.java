da2b57be8675dd40fd7a76125c57c381dcf77e8f
Ángel de Jesús de la Cruz García <82688573+adjcg15@users.noreply.github.com>
2024-05-05 21:37:14 -0600

    FEAT: implementation of gui for auction searching use case
    * feat(SearchAuctionActivity): implements gui for navigation bar, including go back button, searchbar, search button and all related styles
    
    * feat(auction_details_template): implements gui for auction card on search auction view
    
    * fix(activity_search_auction): fixes gravity layout of navigation bar to center content vertically
    
    * feat(activity_search_auction.xml): implements filters list and filter button in gui

---
+++ app/src/main/java/com/bidblast/searchauction/SearchAuctionActivity.java
@@ -0,0 +1,16 @@
+package com.bidblast.searchauction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bidblast.R;
+
+public class SearchAuctionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_auction);
+    }
+}
