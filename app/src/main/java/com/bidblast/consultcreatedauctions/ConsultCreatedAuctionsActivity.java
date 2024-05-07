2b6b8dd9f73a568bdcf21aac756b6a47444c5ffe
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-06 22:34:56 -0600

    feat(activity_consult_created_auctions.xml): implements general view layout with auction details with state template

---
+++ app/src/main/java/com/bidblast/consultcreatedauctions/ConsultCreatedAuctionsActivity.java
@@ -0,0 +1,16 @@
+package com.bidblast.consultcreatedauctions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bidblast.R;
+
+public class ConsultCreatedAuctionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consult_created_auctions);
+    }
+}
