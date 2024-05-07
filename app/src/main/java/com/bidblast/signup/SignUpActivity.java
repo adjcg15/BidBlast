1dd22356b2b380056a0110b7ca2b80fb57a63a4d
Donají Navarro <98196311+DonajiNavarro15@users.noreply.github.com>
2024-05-06 23:53:56 -0600

    FEAT: Implementation of gui for sign up use case
    * feat(signUp): implements graphical interface components
    
    * feat(AndroidManifest): Implements the code for signn up activity

---
+++ app/src/main/java/com/bidblast/signup/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.bidblast.signup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bidblast.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
