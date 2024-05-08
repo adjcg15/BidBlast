6f17dc3c4ea3404a90bf06d499b0309c71b9cd02
Angel Garcia <adjcg15@gmail.com>
2024-05-07 21:12:22 -0600

    feat(login): implements redirection to main menu without any validation

--- app/src/main/java/com/bidblast/login/LoginActivity.java
+++ app/src/main/java/com/bidblast/login/LoginActivity.java
@@ -2,15 +2,34 @@ package com.bidblast.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.bidblast.R;
+import com.bidblast.databinding.ActivityLoginBinding;
+import com.bidblast.databinding.ActivityMainMenuBinding;
+import com.bidblast.mainmenu.MainMenuActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setupLoginButtonClick();
+    }
+
+    private void setupLoginButtonClick() {
+        binding.loginButton.setOnClickListener(v -> {
+            startMainMenuActivity();
+        });
+    }
+
+    private void startMainMenuActivity() {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
