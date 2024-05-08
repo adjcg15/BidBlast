639dbec9c419c4f4fa7b2b82dc7becce905fff25
Angel Garcia <adjcg15@gmail.com>
2024-05-08 15:24:45 -0600

    feat(login): implements logic to show password and email error through view model observer

--- app/src/main/java/com/bidblast/login/LoginActivity.java
+++ app/src/main/java/com/bidblast/login/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.bidblast.R;
 import com.bidblast.databinding.ActivityLoginBinding;
@@ -35,11 +36,23 @@ public class LoginActivity extends AppCompatActivity {
 
     private void setupFieldsValidations() {
         viewModel.isValidEmail().observe(this, isValidEmail -> {
-
+            if (isValidEmail) {
+                binding.emailErrorTextView.setVisibility(View.GONE);
+                binding.emailErrorTextView.setBackgroundResource(R.drawable.basic_input_background);
+            } else {
+                binding.emailErrorTextView.setVisibility(View.VISIBLE);
+                binding.emailErrorTextView.setBackgroundResource(R.drawable.basic_input_error_background);
+            }
         });
 
         viewModel.isValidPassword().observe(this, isValidPassword -> {
-
+            if (isValidPassword) {
+                binding.passwordErrorTextView.setVisibility(View.GONE);
+                binding.passwordErrorTextView.setBackgroundResource(R.drawable.basic_input_background);
+            } else {
+                binding.passwordErrorTextView.setVisibility(View.VISIBLE);
+                binding.passwordErrorTextView.setBackgroundResource(R.drawable.basic_input_error_background);
+            }
         });
     }
 
