559af168f96417d662dcdbf45baa8f3de2b10905
Angel Garcia <adjcg15@gmail.com>
2024-05-08 14:54:05 -0600

    feat(ValidationToolkit.java): implements method to validate if a text is a valid password and changes isValidEmail method to verify that the text is not null

--- app/src/main/java/com/bidblast/lib/ValidationToolkit.java
+++ app/src/main/java/com/bidblast/lib/ValidationToolkit.java
@@ -5,17 +5,26 @@ import java.util.regex.Pattern;
 
 public class ValidationToolkit {
     public static boolean isValidEmail(String text) {
-        String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        boolean isValidEmail = false;
 
-        Pattern pattern = Pattern.compile(emailPattern);
-        Matcher matcher = pattern.matcher(text);
+        if (text != null) {
+            String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 
-        return matcher.matches();
+            Pattern pattern = Pattern.compile(emailPattern);
+            Matcher matcher = pattern.matcher(text);
+
+            isValidEmail = matcher.matches();
+        }
+
+        return isValidEmail;
     }
 
-    public static boolean isValidUserPassword() {
-        boolean isValidUserPassword = true;
+    public static boolean isValidUserPassword(String text) {
+        boolean isValidPassword = false;
 
-        return isValidUserPassword;
+        if (text != null) {
+            isValidPassword = !text.isEmpty();
+        }
+        return isValidPassword;
     }
 }
