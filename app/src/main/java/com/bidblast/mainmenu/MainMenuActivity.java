f30abe11735410a3b1b6c06d0b492bdfe6b382dd
Angel Garcia <adjcg15@gmail.com>
2024-05-07 18:29:40 -0600

    feat(main menu): creates general layout for main menu activity

---
+++ app/src/main/java/com/bidblast/mainmenu/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package com.bidblast.mainmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bidblast.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
