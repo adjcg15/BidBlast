3e06f11d55896a5ffb9f356c2fab6e4b76c00466
Angel Garcia <adjcg15@gmail.com>
2024-05-08 15:07:22 -0600

    feat(LoginActivity.java): implements connection to ViewModel

--- app/src/main/java/com/bidblast/login/LoginActivity.java
+++ app/src/main/java/com/bidblast/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.bidblast.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +12,8 @@ import com.bidblast.databinding.ActivityMainMenuBinding;
 import com.bidblast.mainmenu.MainMenuActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    ActivityLoginBinding binding;
+    private ActivityLoginBinding binding;
+    private LoginViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +21,25 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
         setupLoginButtonClick();
+        setupFieldsValidations();
     }
 
     private void setupLoginButtonClick() {
         binding.loginButton.setOnClickListener(v -> {
             startMainMenuActivity();
+        });
+    }
+
+    private void setupFieldsValidations() {
+        viewModel.isValidEmail().observe(this, isValidEmail -> {
+
+        });
+
+        viewModel.isValidPassword().observe(this, isValidPassword -> {
+
         });
     }
 
