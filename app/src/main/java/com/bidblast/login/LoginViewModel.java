1d770fb6fb58cf56a68e69411f832f304a065367
Angel Garcia <adjcg15@gmail.com>
2024-05-08 14:36:46 -0600

    feat(LoginViewModel.java): defines methods and attributes to be used in login view

--- app/src/main/java/com/bidblast/login/LoginViewModel.java
+++ app/src/main/java/com/bidblast/login/LoginViewModel.java
@@ -1,9 +1,30 @@
 package com.bidblast.login;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
+    private final MutableLiveData<Boolean> isValidEmail = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isValidPassword = new MutableLiveData<>();
 
+    public LoginViewModel() {
 
+    }
+
+    public LiveData<Boolean> isValidEmail() {
+        return isValidEmail;
+    }
+
+    public LiveData<Boolean> isValidPassword() {
+        return isValidPassword;
+    }
+
+    public void validatePassword(String password) {
+
+    }
+
+    public void validateEmail(String password) {
+
+    }
 }
