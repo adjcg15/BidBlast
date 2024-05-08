4c2cc0cd485ce856e5da6e629e8e7577a173f572
Angel Garcia <adjcg15@gmail.com>
2024-05-08 14:40:14 -0600

    feat(ValidationToolkit.java): creates utility class to declare reusable validation methods

---
+++ app/src/main/java/com/bidblast/lib/ValidationToolkit.java
@@ -0,0 +1,15 @@
+package com.bidblast.lib;
+
+public class ValidationToolkit {
+    public static boolean isValidEmail() {
+        boolean isValidEmail = true;
+
+        return isValidEmail;
+    }
+
+    public static boolean isValidUserPassword() {
+        boolean isValidUserPassword = true;
+
+        return isValidUserPassword;
+    }
+}
