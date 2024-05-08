0831375884d193e4d0b194afb70db3de88486fff
Angel Garcia <adjcg15@gmail.com>
2024-05-08 14:58:08 -0600

    feat(LoginViewModel.java): implements validation methods and LiveData values update within them

--- app/src/main/java/com/bidblast/login/LoginViewModel.java
+++ app/src/main/java/com/bidblast/login/LoginViewModel.java
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.bidblast.lib.ValidationToolkit;
+
 public class LoginViewModel extends ViewModel {
     private final MutableLiveData<Boolean> isValidEmail = new MutableLiveData<>();
     private final MutableLiveData<Boolean> isValidPassword = new MutableLiveData<>();
@@ -21,10 +23,14 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void validatePassword(String password) {
+        boolean validationResult = ValidationToolkit.isValidUserPassword(password);
 
+        isValidPassword.setValue(validationResult);
     }
 
-    public void validateEmail(String password) {
+    public void validateEmail(String email) {
+        boolean validationResult = ValidationToolkit.isValidEmail(email);
 
+        isValidEmail.setValue(validationResult);
     }
 }
