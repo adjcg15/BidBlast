0667777c6cac586ab326629c17a93292efbec428
Ángel de Jesús de la Cruz García <82688573+adjcg15@users.noreply.github.com>
2024-05-07 20:45:06 -0600

    FEAT: main bottom navigation menu implementation
    * feat(main menu): creates general layout for main menu activity
    
    * feat(main menu): implements first approach to handle redirections using fragments

---
+++ app/src/main/java/com/bidblast/mainmenu/MainMenuActivity.java
@@ -0,0 +1,53 @@
+package com.bidblast.mainmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.bidblast.R;
+import com.bidblast.databinding.ActivityMainMenuBinding;
+
+public class MainMenuActivity extends AppCompatActivity {
+    ActivityMainMenuBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setupMenuNavigation();
+    }
+
+    private void setupMenuNavigation() {
+        int searchMenuItemId = R.id.searchMenuItem;
+        int purchasesMenuItemId = R.id.purchasesMenuItem;
+        int salesMenuItemId = R.id.salesMenuItem;
+        int newAuctionMenuItemId = R.id.newAuctionMenuItem;
+
+        binding.mainMenuBottomNavigationView.setOnItemReselectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == searchMenuItemId) {
+                //TODO: show seach auction fragment
+            } else if (itemId == purchasesMenuItemId) {
+                //TODO: show purchases fragment
+            } else if (itemId == salesMenuItemId) {
+                //TODO: show sales fragmet
+            } else if (itemId == newAuctionMenuItemId) {
+                //TODO: show create auction fragment
+            }
+        });
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(binding.mainViewFragmentLayout.getId(), fragment);
+        fragmentTransaction.commit();
+    }
+}
