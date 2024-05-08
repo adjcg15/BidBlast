819aaf8e335aad3a2888992fa6818d1831b95677
Donají Navarro <98196311+DonajiNavarro15@users.noreply.github.com>
2024-05-08 04:49:47 -0600

    FEAT: configuration of necessary elements for view of the use case
    * fix(signUp): corrects screen dimensions to show "register" button
    
    * fix(signUp): modifies the icons used in the gui
    
    * feat(postItem): configures necessary elements for view of the use case

---
+++ app/src/main/java/com/bidblast/postitem/PostItemActivity.java
@@ -0,0 +1,15 @@
+package com.bidblast.postitem;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bidblast.R;
+
+public class PostItemActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_item_for_auction);
+        setContentView(R.layout.activity_post_item_for_auction_second_part);
+    }
+}
