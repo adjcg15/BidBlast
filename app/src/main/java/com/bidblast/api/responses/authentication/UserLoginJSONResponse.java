8ed366b7f8e36b06a2da0fe9d85122aab1ca2c33
Angel Garcia <adjcg15@gmail.com>
2024-05-08 18:06:32 -0600

    fix(UserLoginJSONResponse.java): implements getters for every attribute defined in the class

--- app/src/main/java/com/bidblast/api/responses/authentication/UserLoginJSONResponse.java
+++ app/src/main/java/com/bidblast/api/responses/authentication/UserLoginJSONResponse.java
@@ -1,11 +1,41 @@
 package com.bidblast.api.responses.authentication;
 
+import java.util.List;
+
 public class UserLoginJSONResponse {
     private int id;
     private String fullName;
     private String phoneNumber;
     private String avatar;
     private String email;
-    private String[] roles;
+    private List<String> roles;
     private String token;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
