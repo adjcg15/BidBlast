b96e7aa2c75201db8ebf7a24c2fa5293acacd022
Angel Garcia <adjcg15@gmail.com>
2024-05-09 19:31:16 -0600

    feat(Session.java): creates singleton to store all user information after login

---
+++ app/src/main/java/com/bidblast/lib/Session.java
@@ -0,0 +1,36 @@
+package com.bidblast.lib;
+
+import com.bidblast.model.User;
+
+public class Session {
+    private static Session instance;
+    private String token;
+
+    private User user;
+
+    private Session() { }
+
+    public Session getInstance() {
+        if(instance == null) {
+            instance = new Session();
+        }
+
+        return instance;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
