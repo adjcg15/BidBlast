b7fdab9367a4faf325d50845d50f58c7c4fde3e9
Angel Garcia <adjcg15@gmail.com>
2024-05-12 17:37:32 -0600

    feat(HypermediaFile.java): creates HypermediaFile model

---
+++ app/src/main/java/com/bidblast/model/HypermediaFile.java
@@ -0,0 +1,49 @@
+package com.bidblast.model;
+
+public class HypermediaFile {
+    private int id;
+    private String content;
+    private String name;
+    private String mimeType;
+
+    public HypermediaFile() { }
+
+    public HypermediaFile(int id, String content, String name, String mimeType) {
+        this.id = id;
+        this.content = content;
+        this.name = name;
+        this.mimeType = mimeType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+}
