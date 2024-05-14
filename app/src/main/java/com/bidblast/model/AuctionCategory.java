0e2a70af8058fa43265eddfcbb128e7fb6388cdb
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-13 22:11:15 -0600

    feat(ModifyAuctionCategory): implements text fields validation using ViewModel and change AuctionCategory.java to implements Parcelable

--- app/src/main/java/com/bidblast/model/AuctionCategory.java
+++ app/src/main/java/com/bidblast/model/AuctionCategory.java
@@ -1,23 +1,47 @@
 package com.bidblast.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.util.Objects;
 
-public class AuctionCategory {
+public class AuctionCategory implements Parcelable {
     private int id;
 
     private String title;
 
     private String description;
-    private String keyWords;
+    private String keywords;
 
     public AuctionCategory() {}
 
-    public AuctionCategory(int id, String title, String description, String keyWords) {
+    public AuctionCategory(int id, String title, String description, String keywords) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.keyWords = keyWords;
+        this.keywords = keywords;
     }
+
+    protected AuctionCategory(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        description = in.readString();
+        keywords = in.readString();
+    }
+
+    public static final Creator<AuctionCategory> CREATOR = new Creator<AuctionCategory>() {
+        @Override
+        public AuctionCategory createFromParcel(Parcel in) {
+            return new AuctionCategory(in);
+        }
+
+        @Override
+        public AuctionCategory[] newArray(int size) {
+            return new AuctionCategory[size];
+        }
+    };
 
     public int getId() {
         return id;
@@ -43,12 +67,12 @@ public class AuctionCategory {
         this.description = description;
     }
 
-    public String getKeyWords() {
-        return keyWords;
+    public String getKeywords() {
+        return keywords;
     }
 
-    public void setKeyWords(String keyWords) {
-        this.keyWords = keyWords;
+    public void setKeywords(String keyWords) {
+        this.keywords = keyWords;
     }
 
     @Override
@@ -56,11 +80,24 @@ public class AuctionCategory {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         AuctionCategory that = (AuctionCategory) o;
-        return id == that.id && Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(keyWords, that.keyWords);
+        return id == that.id && Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(keywords, that.keywords);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, description, keyWords);
+        return Objects.hash(id, title, description, keywords);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(keywords);
     }
 }
--- app/src/main/java/com/bidblast/usecases/modifycategory/ModifyAuctionCategoryFragment.java
+++ app/src/main/java/com/bidblast/usecases/modifycategory/ModifyAuctionCategoryFragment.java
@@ -3,31 +3,64 @@ package com.bidblast.usecases.modifycategory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bidblast.R;
+import com.bidblast.databinding.FragmentModifyAuctionCategoryBinding;
+import com.bidblast.usecases.login.LoginViewModel;
 
 public class ModifyAuctionCategoryFragment extends Fragment {
 
-    public ModifyAuctionCategoryFragment() {
+    private FragmentModifyAuctionCategoryBinding binding;
+    private ModifyAuctionCategoryViewModel viewModel;
 
-    }
+    private static final String ID_KEY = "id";
+    private static final String TITLE_KEY = "title";
+    private static final String DESCRIPTION_KEY = "description";
+    private static final String KEYWORDS_KEY = "keywords";
 
-    public static ModifyAuctionCategoryFragment newInstance(String param1, String param2) {
-        return new ModifyAuctionCategoryFragment();
+    private int id;
+    private String title;
+    private String description;
+    private String keywords;
+
+    public ModifyAuctionCategoryFragment() {}
+
+    public static ModifyAuctionCategoryFragment newInstance(int id, String title, String description, String keywords) {
+        ModifyAuctionCategoryFragment fragment = new ModifyAuctionCategoryFragment();
+        Bundle args = new Bundle();
+        args.putInt(ID_KEY, id);
+        args.putString(TITLE_KEY, title);
+        args.putString(DESCRIPTION_KEY, description);
+        args.putString(KEYWORDS_KEY, keywords);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            id = getArguments().getInt(ID_KEY);
+            title = getArguments().getString(TITLE_KEY);
+            description = getArguments().getString(DESCRIPTION_KEY);
+            keywords = getArguments().getString(KEYWORDS_KEY);
+            // Aquí podrías hacer cualquier cosa que necesites con los datos, como pasárselos al ViewModel
+            // viewModel.setData(id, title, description, keywords);
+        }
+
+        viewModel = new ViewModelProvider(this).get(ModifyAuctionCategoryViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_modify_auction_category, container, false);
+        binding = FragmentModifyAuctionCategoryBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
+
