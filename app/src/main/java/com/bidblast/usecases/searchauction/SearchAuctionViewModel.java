34008a13298d63e0c6ed9b45fff62935ecd465fc
Angel Garcia <adjcg15@gmail.com>
2024-05-14 23:30:41 -0600

    feat(SearchAuctionViewModel.java): implements all logic related to price filters

--- app/src/main/java/com/bidblast/usecases/searchauction/SearchAuctionViewModel.java
+++ app/src/main/java/com/bidblast/usecases/searchauction/SearchAuctionViewModel.java
@@ -6,12 +6,14 @@ import androidx.lifecycle.MutableLiveData;
 import com.bidblast.api.RequestStatus;
 import com.bidblast.model.Auction;
 import com.bidblast.model.AuctionCategory;
+import com.bidblast.model.PriceRange;
 import com.bidblast.repositories.AuctionCategoriesRepository;
 import com.bidblast.repositories.AuctionsRepository;
 import com.bidblast.repositories.IProcessStatusListener;
 import com.bidblast.repositories.ProcessErrorCodes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,6 +25,8 @@ public class SearchAuctionViewModel {
     private final MutableLiveData<RequestStatus> auctionCategoriesListRequestStatus = new MutableLiveData<>();
     private final MutableLiveData<List<Integer>> categoryFiltersSelected =
         new MutableLiveData<>(new ArrayList<>());
+    private final MutableLiveData<PriceRange> priceFilterSelected =
+        new MutableLiveData<>();
 
     public LiveData<List<Auction>> getAuctionsList() { return auctionsList; }
 
@@ -41,6 +45,8 @@ public class SearchAuctionViewModel {
     public LiveData<List<Integer>> getCategoryFiltersSelected() {
         return categoryFiltersSelected;
     }
+
+    public LiveData<PriceRange> getPriceFilterSelected() { return priceFilterSelected; }
 
     public void recoverAuctions(String searchQuery, int limit, int offset) {
         auctionsListRequestStatus.setValue(RequestStatus.LOADING);
@@ -81,6 +87,18 @@ public class SearchAuctionViewModel {
         );
     }
 
+    public List<PriceRange> getAllPriceRanges() {
+        return new ArrayList<>(Arrays.asList(
+            new PriceRange("Menos de $100", Float.NEGATIVE_INFINITY, 100.0f),
+            new PriceRange("$100 a menos de $200", 100.0f, 200.0f),
+            new PriceRange("$200 a menos de $300", 200.0f, 300.0f),
+            new PriceRange("$300 a menos de $500", 300.0f, 500.0f),
+            new PriceRange("$500 a menos de $750", 500.0f, 750.0f),
+            new PriceRange("$750 a menos de $1000", 750.0f, 1000.0f),
+            new PriceRange("$1000 o más", 1000.0f, Float.POSITIVE_INFINITY)
+        ));
+    }
+
     public void toggleCategoryFilter(AuctionCategory category) {
         Integer categoryId = category.getId();
 
@@ -92,5 +110,13 @@ public class SearchAuctionViewModel {
         }
 
         categoryFiltersSelected.setValue(currentFilters);
+    }
+
+    public void togglePriceFilter(PriceRange priceRange) {
+        if(priceRange.equals(priceFilterSelected.getValue())) {
+            priceFilterSelected.setValue(null);
+        } else {
+            priceFilterSelected.setValue(priceRange);
+        }
     }
 }
