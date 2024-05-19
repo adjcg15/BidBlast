a946d634b18b24c8cdbe8354385583d12823632f
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-18 19:08:56 -0600

    feat(CompletedAuctionDetailsAdapter): implements CompletedAuctionDetailsAdapter.java

--- app/src/main/java/com/bidblast/usecases/consultcompletedauctions/ConsultCompletedAuctionsFragment.java
+++ app/src/main/java/com/bidblast/usecases/consultcompletedauctions/ConsultCompletedAuctionsFragment.java
@@ -3,30 +3,40 @@ package com.bidblast.usecases.consultcompletedauctions;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bidblast.R;
+import com.bidblast.databinding.FragmentConsultCompletedAuctionsBinding;
 
 public class ConsultCompletedAuctionsFragment extends Fragment {
+    private static final int TOTAL_AUCTIONS_TO_LOAD = 5;
+    private FragmentConsultCompletedAuctionsBinding binding;
+    private ConsultCompletedAuctionsViewModel viewModel;
+    private CompletedAuctionDetailsAdapter completedAuctionDetailsAdapter;
     public ConsultCompletedAuctionsFragment() {
 
     }
 
-    public static ConsultCompletedAuctionsFragment newInstance(String param1, String param2) {
+    public static ConsultCompletedAuctionsFragment newInstance() {
         return new ConsultCompletedAuctionsFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(ConsultCompletedAuctionsViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_consult_completed_auctions, container, false);
+        binding = FragmentConsultCompletedAuctionsBinding.inflate(inflater, container, false);
+        completedAuctionDetailsAdapter = new CompletedAuctionDetailsAdapter(getContext());
+        binding.completedAuctionsListRecyclerView.setAdapter(completedAuctionDetailsAdapter);
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/bidblast/usecases/consultcompletedauctions/ConsultCompletedAuctionsViewModel.java
+++ app/src/main/java/com/bidblast/usecases/consultcompletedauctions/ConsultCompletedAuctionsViewModel.java
@@ -2,6 +2,7 @@ package com.bidblast.usecases.consultcompletedauctions;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 import com.bidblast.api.RequestStatus;
 import com.bidblast.model.Auction;
@@ -12,7 +13,7 @@ import com.bidblast.repositories.ProcessErrorCodes;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConsultCompletedAuctionsViewModel {
+public class ConsultCompletedAuctionsViewModel extends ViewModel {
     private final MutableLiveData<List<Auction>> auctionsList = new MutableLiveData<>(new ArrayList<>());
     private final MutableLiveData<RequestStatus> auctionsListRequestStatus = new MutableLiveData<>();
     private final MutableLiveData<Boolean> stillAuctionsLeftToLoad = new MutableLiveData<>(true);
