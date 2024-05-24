0cf535b286624e49a56f207a0ca17764c0aa9ffa
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-24 10:59:11 -0600

    feat(ConsultCreatedAuctions): implements CreatedAuctionDetailsAdapter without methods

--- app/src/main/java/com/bidblast/usecases/consultcreatedauctions/ConsultCreatedAuctionsFragment.java
+++ app/src/main/java/com/bidblast/usecases/consultcreatedauctions/ConsultCreatedAuctionsFragment.java
@@ -7,32 +7,163 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bidblast.R;
+import com.bidblast.api.RequestStatus;
+import com.bidblast.databinding.FragmentConsultCompletedAuctionsBinding;
+import com.bidblast.model.Auction;
+import com.bidblast.usecases.consultcompletedauctions.CompletedAuctionDetailsAdapter;
+import com.bidblast.usecases.consultcompletedauctions.ConsultCompletedAuctionsFragment;
+import com.bidblast.usecases.consultcompletedauctions.ConsultCompletedAuctionsViewModel;
+
+import java.util.List;
 
 public class ConsultCreatedAuctionsFragment extends Fragment {
 
-    private ConsultCreatedAuctionsViewModel mViewModel;
+    private static final int TOTAL_AUCTIONS_TO_LOAD = 5;
+    private FragmentConsultCompletedAuctionsBinding binding;
+    private ConsultCompletedAuctionsViewModel viewModel;
+    private CreatedAuctionDetailsAdapter createdAuctionDetailsAdapter;
+    private String searchQuery;
+    public ConsultCreatedAuctionsFragment() {
 
-    public static ConsultCreatedAuctionsFragment newInstance() {
-        return new ConsultCreatedAuctionsFragment();
+    }
+
+    public static ConsultCompletedAuctionsFragment newInstance() {
+        return new ConsultCompletedAuctionsFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_consult_created_auctions, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(ConsultCompletedAuctionsViewModel.class);
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        //mViewModel = new ViewModelProvider(this).get(ConsultCreatedAuctionsViewModel.class);
-        // TODO: Use the ViewModel
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentConsultCompletedAuctionsBinding.inflate(inflater, container, false);
+        createdAuctionDetailsAdapter = new CreatedAuctionDetailsAdapter(getContext());
+        binding.completedAuctionsListRecyclerView.setAdapter(createdAuctionDetailsAdapter);
+
+        setupCompletedAuctionsListStatusListener();
+        setupCompletedAuctionsListListener();
+        setupRecyclerViewScrollListener();
+        setupStillCompletedAuctionsLeftToLoadListener();
+        setupSearchCompletedAuctionsImageButton();
+        loadCompletedAuctions();
+        return binding.getRoot();
     }
 
+    private void setupSearchCompletedAuctionsImageButton() {
+        binding.searchAuctionsImageButton.setOnClickListener(v -> {
+            viewModel.cleanAuctionsList();
+            loadCompletedAuctions();
+        });
+    }
+
+    private void loadCompletedAuctions() {
+        searchQuery = binding.searchBarEditText.getText().toString();
+
+        viewModel.recoverAuctions(searchQuery, TOTAL_AUCTIONS_TO_LOAD);
+    }
+
+    private void setupCompletedAuctionsListStatusListener() {
+        viewModel.getAuctionsListRequestStatus().observe(getViewLifecycleOwner(), requestStatus -> {
+            binding.emptyCompletedAuctionsMessageLinearLayout.setVisibility(View.GONE);
+
+            if(requestStatus == RequestStatus.LOADING) {
+                binding.loadingAuctionsTextView.setVisibility(View.VISIBLE);
+            } else {
+                binding.loadingAuctionsTextView.setVisibility(View.GONE);
+
+                if(requestStatus == RequestStatus.DONE) {
+                    binding.errorLoadingCompletedAuctionsLinearLayout.setVisibility(View.GONE);
+
+                    List<Auction> auctions = viewModel.getAuctionsList().getValue();
+                    if(auctions != null && auctions.size() != 0) {
+                        binding.completedAuctionsListRecyclerView.setVisibility(View.VISIBLE);
+                        binding.emptyCompletedAuctionsMessageLinearLayout.setVisibility(View.GONE);
+                    } else {
+                        if (searchQuery == null || searchQuery.isEmpty()) {
+                            binding.emptyCompletedAuctionsTitleTextView.setText(
+                                    getString(
+                                            R.string.consultcompletedauctions_empty_completed_auctions_title
+                                    )
+                            );
+                            binding.emptyCompletedAuctionsTextTextView.setText(
+                                    getString(
+                                            R.string.consultcompletedauctions_empty_completed_auctions_text
+                                    )
+                            );
+                        } else {
+                            binding.emptyCompletedAuctionsTitleTextView.setText(
+                                    getString(
+                                            R.string.consultcompletedauctions_empty_completed_auctions_by_search_title
+                                    )
+                            );
+                            binding.emptyCompletedAuctionsTextTextView.setText(
+                                    getString(
+                                            R.string.consultcompletedauctions_empty_completed_auctions_by_search_text
+                                    )
+                            );
+                        }
+                        binding.completedAuctionsListRecyclerView.setVisibility(View.GONE);
+                        binding.emptyCompletedAuctionsMessageLinearLayout.setVisibility(View.VISIBLE);
+                        binding.allAuctionsLoadedTextView.setVisibility(View.GONE);
+                    }
+                } else if (requestStatus == RequestStatus.ERROR) {
+                    binding.errorLoadingCompletedAuctionsLinearLayout.setVisibility(View.VISIBLE);
+                    binding.completedAuctionsListRecyclerView.setVisibility(View.GONE);
+                    binding.allAuctionsLoadedTextView.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private void setupStillCompletedAuctionsLeftToLoadListener() {
+        viewModel.getStillAuctionsLeftToLoad().observe(getViewLifecycleOwner(), stillAuctionsLeftToLoad -> {
+            if(stillAuctionsLeftToLoad) {
+                binding.allAuctionsLoadedTextView.setVisibility(View.GONE);
+            } else {
+                binding.allAuctionsLoadedTextView.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void setupCompletedAuctionsListListener() {
+        viewModel.getAuctionsList().observe(getViewLifecycleOwner(), auctionsList -> {
+            createdAuctionDetailsAdapter.submitList(auctionsList);
+        });
+    }
+
+    private void setupRecyclerViewScrollListener() {
+        binding.completedAuctionsListRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                if (layoutManager != null) {
+                    int visibleItemCount = layoutManager.getChildCount();
+                    int totalItemCount = layoutManager.getItemCount();
+                    int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
+                    if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                            && firstVisibleItemPosition >= 0
+                            && totalItemCount >= TOTAL_AUCTIONS_TO_LOAD
+                            && Boolean.TRUE.equals(viewModel.getStillAuctionsLeftToLoad().getValue())
+                            && viewModel.getAuctionsListRequestStatus().getValue() != RequestStatus.LOADING) {
+                        loadCompletedAuctions();
+                    }
+                }
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/bidblast/usecases/consultcreatedauctions/CreatedAuctionDetailsAdapter.java
@@ -0,0 +1,85 @@
+package com.bidblast.usecases.consultcreatedauctions;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bidblast.R;
+import com.bidblast.databinding.TemplateAuctionDetailsWithStateBinding;
+import com.bidblast.lib.CurrencyToolkit;
+import com.bidblast.lib.DateToolkit;
+import com.bidblast.lib.ImageToolkit;
+import com.bidblast.model.Auction;
+import com.bidblast.model.HypermediaFile;
+import com.bidblast.model.Offer;
+import com.bidblast.model.User;
+import com.bidblast.usecases.consultcompletedauctions.CompletedAuctionDetailsAdapter;
+
+public class CreatedAuctionDetailsAdapter extends ListAdapter<Auction, CreatedAuctionDetailsAdapter.CreatedAuctionViewHolder> {
+    private Context context;
+
+    public static final DiffUtil.ItemCallback<Auction> DIFF_CALLBACK =
+            new DiffUtil.ItemCallback<Auction>() {
+                @Override
+                public boolean areItemsTheSame(@NonNull Auction oldItem, @NonNull Auction newItem) {
+                    return oldItem.getId() == newItem.getId();
+                }
+
+                @Override
+                public boolean areContentsTheSame(@NonNull Auction oldItem, @NonNull Auction newItem) {
+                    return oldItem.equals(newItem);
+                }
+            };
+
+    protected CreatedAuctionDetailsAdapter(Context context) {
+        super(DIFF_CALLBACK);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CreatedAuctionDetailsAdapter.CreatedAuctionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TemplateAuctionDetailsWithStateBinding binding =
+                TemplateAuctionDetailsWithStateBinding.inflate(LayoutInflater.from(parent.getContext()));
+
+        return new CreatedAuctionDetailsAdapter.CreatedAuctionViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CreatedAuctionDetailsAdapter.CreatedAuctionViewHolder holder, int position) {
+        Auction auction = getItem(position);
+        holder.bind(auction);
+        holder.binding.auctioneerPhoneNumberImageButton.setOnClickListener(v -> {
+            ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("label", auction.getAuctioneer().getPhoneNumber());
+            clipboard.setPrimaryClip(clip);
+        });
+        holder.binding.auctioneerEmailImageButton.setOnClickListener(v -> {
+            ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("label", auction.getAuctioneer().getEmail());
+            clipboard.setPrimaryClip(clip);
+        });
+    }
+
+    public static class CreatedAuctionViewHolder extends RecyclerView.ViewHolder {
+        private final TemplateAuctionDetailsWithStateBinding binding;
+
+        public CreatedAuctionViewHolder(@NonNull TemplateAuctionDetailsWithStateBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(Auction auction) {
+
+            binding.executePendingBindings();
+        }
+    }
+}
