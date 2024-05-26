e3f05d15193bb923bc37788e7030cadbd7599b44
Angel Garcia <adjcg15@gmail.com>
2024-05-26 14:20:54 -0600

    feat(moderator menu activity): implements logic to show use case fragment depending on menu option selected

---
+++ app/src/main/java/com/bidblast/menus/moderatormenu/ModeratorMenuActivity.java
@@ -0,0 +1,58 @@
+package com.bidblast.menus.moderatormenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.bidblast.R;
+import com.bidblast.databinding.ActivityMainMenuBinding;
+import com.bidblast.databinding.ActivityModeratorMenuBinding;
+import com.bidblast.usecases.consultcompletedauctions.ConsultCompletedAuctionsFragment;
+import com.bidblast.usecases.consultcreatedauctions.ConsultCreatedAuctionsFragment;
+import com.bidblast.usecases.searchauction.SearchAuctionFragment;
+
+public class ModeratorMenuActivity extends AppCompatActivity {
+    ActivityModeratorMenuBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityModeratorMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //TODO: show initial use case fragment
+        //showFragment(new InitialUseCaseFragment());
+        setupMenuNavigation();
+    }
+
+    private void setupMenuNavigation() {
+        int categoriesMenuItemId = R.id.categoriesMenuItem;
+        int auctionsMenuItemId = R.id.auctionsMenuItem;
+        int statisticsMenuItemId = R.id.statisticsMenuItem;
+
+        binding.mainMenuBottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == categoriesMenuItemId) {
+                //TODO: show categories fragment
+            } else if (itemId == auctionsMenuItemId) {
+                //TODO: show auctions fragment
+            } else if (itemId == statisticsMenuItemId) {
+                //TODO: show statistics fragment
+            }
+
+            return true;
+        });
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(binding.mainViewFragmentLayout.getId(), fragment);
+        fragmentTransaction.commit();
+    }
+}
