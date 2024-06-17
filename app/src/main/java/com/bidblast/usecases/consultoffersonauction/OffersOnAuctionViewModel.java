e272ca93c7a244ccdc3a20dc9dd6caf9ed78c444
rodrigo19112003 <131178299+rodrigo19112003@users.noreply.github.com>
2024-06-17 10:45:37 -0600

    FEAT: Implementation recover offers and show them on recycler view
    * feat(offers): implements template offer details and show its on fragment offers on auction
    
    * feat(offers): implements offer details adapter to handle template offer details
    
    * feat(offers): implements get offers on auction and show them on recycler view
    
    * feat(offers): implements get offers on auction and show them on recycler view

--- app/src/main/java/com/bidblast/usecases/consultoffersonauction/OffersOnAuctionViewModel.java
+++ app/src/main/java/com/bidblast/usecases/consultoffersonauction/OffersOnAuctionViewModel.java
@@ -1,21 +1,29 @@
 package com.bidblast.usecases.consultoffersonauction;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.bidblast.api.RequestStatus;
 import com.bidblast.model.Auction;
+import com.bidblast.model.Offer;
 import com.bidblast.repositories.AuctionsRepository;
 import com.bidblast.repositories.IProcessStatusListener;
 import com.bidblast.repositories.ProcessErrorCodes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OffersOnAuctionViewModel extends ViewModel {
     private final MutableLiveData<Auction> auction = new MutableLiveData<>();
     private final MutableLiveData<RequestStatus> auctionRequestStatus = new MutableLiveData<>();
     private final MutableLiveData<ProcessErrorCodes> auctionErrorCode = new MutableLiveData<>();
+    private final MutableLiveData<List<Offer>> offersList = new MutableLiveData<>(new ArrayList<>());
+
+    private final MutableLiveData<Boolean> stillOffersLeftToLoad = new MutableLiveData<>(true);
+    private final MutableLiveData<RequestStatus> offersListRequestStatus = new MutableLiveData<>();
 
     public LiveData<Auction> getAuction() { return auction; }
     public LiveData<ProcessErrorCodes> getAuctionErrorCode() {
@@ -25,6 +33,8 @@ public class OffersOnAuctionViewModel extends ViewModel {
     public LiveData<RequestStatus> getAuctionRequestStatus() {
         return auctionRequestStatus;
     }
+    public LiveData<Boolean> getStillOffersLeftToLoad() { return stillOffersLeftToLoad; }
+
     public void recoverAuction(int idAuction) {
         auctionRequestStatus.setValue(RequestStatus.LOADING);
 
@@ -41,6 +51,43 @@ public class OffersOnAuctionViewModel extends ViewModel {
                     public void onError(ProcessErrorCodes errorCode) {
                         auctionErrorCode.setValue(errorCode);
                         auctionRequestStatus.setValue(RequestStatus.ERROR);
+                    }
+                }
+        );
+    }
+
+    public LiveData<List<Offer>> getOffersList() { return offersList; }
+
+    public LiveData<RequestStatus> getOffersListRequestStatus() {
+        return offersListRequestStatus;
+    }
+
+    public void recoverOffers(int idAuction, int limit) {
+        offersListRequestStatus.setValue(RequestStatus.LOADING);
+        int totalAuctionsLoaded = offersList.getValue() != null
+                ? offersList.getValue().size()
+                : 0;
+
+        new AuctionsRepository().getUserAuctionOffersByAuctionId(
+                idAuction, limit, totalAuctionsLoaded,
+                new IProcessStatusListener<List<Offer>>() {
+
+                    @Override
+                    public void onSuccess(List<Offer> offers) {
+                        List<Offer> currentOffers = new ArrayList<>(offersList.getValue());
+                        currentOffers.addAll(offers);
+                        offersList.setValue(currentOffers);
+
+                        if (offers.size() < limit) {
+                            stillOffersLeftToLoad.setValue(false);
+                        }
+
+                        offersListRequestStatus.setValue(RequestStatus.DONE);
+                    }
+
+                    @Override
+                    public void onError(ProcessErrorCodes errorCode) {
+                        offersListRequestStatus.setValue(RequestStatus.ERROR);
                     }
                 }
         );
